684d6cf753e9c6bd913db652c22f0725a5ff67bd
Leo Gisselberg <leo.gisselberg@digitalspark.se>
2018-11-23 16:01:49 +0100

    Created basic redux operations

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
 import axios from 'axios'
+import reducers from './reducers'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 axios.defaults.withCredentials = true
 axios.defaults.baseURL = 'https://rem-rest-api.herokuapp.com/'
 
+const store = createStore(reducers)
+
 ReactDOM.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 , document.getElementById('root'))
 
 serviceWorker.unregister()
